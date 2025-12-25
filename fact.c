788eec95354cd8a863c1aaf1b64c9fd8dc62b706
faaizxnova <faaizxnova@gmail.com>
2025-12-25 23:41:57 +0530

    Remove factorial function from fact.c
    Removed the recursive factorial function implementation.

--- fact.c
+++ fact.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-int fact(int num) {
-    if (num == 1) return 1;
-    return num * fact(num - 1);
-}
-
 void main() {
     int num;
     scanf("%d", &num);
-    printf("%d", fact(num));
+
 }
